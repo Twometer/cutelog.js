6093d7d852c0068a2db8be2c2b318f2c71808fe5
Twometer <twometer@outlook.de>
2021-06-14 01:20:44 +0200

    v1.0.2: Declare definitions

---
+++ src/main.d.ts
@@ -0,0 +1,11 @@
+export declare function info(message: string)
+
+export declare function okay(message: string)
+
+export declare function warn(message: string)
+
+export declare function error(message: string)
+
+export declare function configure(levelName: string, formatter: (string) => string)
+
+export declare function custom(levelName: string, message: string)
